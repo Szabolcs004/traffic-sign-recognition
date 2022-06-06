a0dac4c72e2596797416e797761e24e31a2e0dcb
Szabolcs004 <JanvariSz2004@gmail.com>
2022-06-06 14:16:41 +0200

    Version 2.0

--- traffic-sign-recognition/main.py
+++ traffic-sign-recognition/main.py
@@ -1,70 +1,47 @@
 import cv2
 import numpy as np
 
-frameWidth = 640
-frameHeight = 480
+thres = 0.45  # Threshold to detect object
+nms_threshold = 0.2
 cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
+# cap.set(3,1280)
+# cap.set(4,720)
+# cap.set(10,150)
 
+classNames = []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('n').split('n')
 
-def empty(a):
-    pass
+    # print(classNames)
+    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+    weightsPath = 'frozen_inference_graph.pb'
 
+    net = cv2.dnn_DetectionModel(weightsPath, configPath)
+    net.setInputSize(320)
+    net.setInputScale(1.0 / 127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
 
-cv2.nameWindow('Parameters')
-cv2.resizeWindow('Parameters', 640, 240)
-cv2.createTrackbar('Threshold1', 'Parameters', 150, 255, empty)
-cv2.createTrackbar('Threshold2', 'Parameters', 255, 255, empty)
+    while True:
+        success, img = cap.read()
+        classIds, confs, bbox = net.detect(img, confThreshold=thres)
+        bbox = list(bbox)
+        confs = list(np.array(confs).reshape(1, -1)[0])
+        confs = list(map(float, confs))
+        # print(type(confs[0]))
+        # print(confs)
 
+        indices = cv2.dnn.NMSBoxes(bbox, confs, thres, nms_threshold)
+        # print(indices)
 
-def stackImages(scale, imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range(0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
-                                                None, scale, scale)
-                if len(imgArray[x][y].shape) == 2:
-                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
-            if len(imgArray[x].shape) == 2:
-                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor = np.hstack(imgArray)
-        ver = hor
-    return ver
+        for i in indices:
+            i = i[0]
+            box = bbox[i]
+            x, y, w, h = box[0], box[1], box[2], box[3]
+            cv2.rectangle(img, (x, y), (x + w, h + y), color=(0, 255, 0), thickness=2)
+            cv2.putText(img, classNames[classIds[i][0] - 1].upper(), (box[0] + 10, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
-
-while True:
-    success, img = cap.read()
-
-    imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
-    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-
-    threshold1 = cv2.getTrackbarPos('Threshold1', 'Parameters')
-    threshold2 = cv2.getTrackbarPos('Threshold2', 'Parameters')
-    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
-
-
-    imgStack = stackImages(0.8, ([img, imgBlur, imgGray]))
-    cv2.imshow("Result", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow('Output', img)
+        cv2.waitKey(1)
